1a3ac7bc1c93d5edf6b45d08cd1ea2ac0bf897bd
Anuja Mutalik <a.mutalik@zera.de>
2022-12-08 14:27:29 +0100

    Some more tests
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- tests/gtest/unittest-fileaccesshelper.cpp
+++ tests/gtest/unittest-fileaccesshelper.cpp
@@ -3,9 +3,11 @@
 
 static const QString accessAllowedFile = QStringLiteral("../test-data/access-allowed.txt");
 static const QString accessNotAllowedFile = QStringLiteral("../test-data/access-not-allowed.txt");
+static const QString nonExistingFile = QStringLiteral("../test-data/foo.txt");
 
 static const QString accessAllowedFolder =  QStringLiteral("../test-data/operator/logger/");
 static const QString accessNotAllowedFolder =  QStringLiteral("../test-data/operator/license-key/");
+static const QString nonExistingFolder =  QStringLiteral("../test-data/operator/foo/");
 
 TEST(FILE_ACCESS, EMPTY_ALLOWED_LISTS)
 {
@@ -27,6 +29,13 @@ TEST(FILE_ACCESS, FILE_WITH_ACCESS_NOT_ALLOWED)
     EXPECT_FALSE(testAccess.isFileAccessAllowed(accessNotAllowedFile));
 }
 
+TEST(FILE_ACCESS, NON_EXISTING_FILE)
+{
+    FileAccessHelper testAccess;
+    testAccess.addFileToAllowedFilesList(accessAllowedFile);
+    EXPECT_FALSE(testAccess.isFileAccessAllowed(nonExistingFile));
+}
+
 TEST(FILE_ACCESS, FOLDER_WITH_ACCESS_ALLOWED)
 {
     FileAccessHelper testAccess;
@@ -39,4 +48,11 @@ TEST(FILE_ACCESS, FOLDER_WITH_ACCESS_NOT_ALLOWED)
     FileAccessHelper testAccess;
     testAccess.addDirToAllowedDirList(accessAllowedFolder);
     EXPECT_FALSE(testAccess.isFileAccessAllowed(accessNotAllowedFolder + QStringLiteral("test.lic")));
+}
+
+TEST(FILE_ACCESS, NON_EXISTING_FOLDER)
+{
+    FileAccessHelper testAccess;
+    testAccess.addDirToAllowedDirList(accessAllowedFolder);
+    EXPECT_FALSE(testAccess.isFileAccessAllowed(nonExistingFolder + QStringLiteral("bar.lic")));
 }
